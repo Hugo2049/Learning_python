64c7f0b3f0c9265bc00fcdc1c3a86b0440e6eadc
Hb_W <hugobarillasvi@hotmail.com>
2025-03-08 21:34:00 -0600

    Varibles declaration

---
+++ variables.py
@@ -0,0 +1,16 @@
+
+"""First of all learn about variablej
+  Varibles as I understood are like containers for values, like something that store the information of something
+  In python you can create variables like: """
+
+a = 2
+b = 8
+
+c = 'hi'
+d = str(3)
+
+e = True 
+f = False
+
+print(a)
+print(e)
