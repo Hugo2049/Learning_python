38b1209d259e697df5f6b45cc4731118356f0e79
Hb_W <hugobarillasvi@hotmail.com>
2025-03-09 20:11:38 -0600

    conditional if

---
+++ conditionals.py
@@ -0,0 +1,22 @@
+"""Starting with if, it works like this
+   if <expr>:
+        <statement>
+    else: 
+        <statement>"""
+
+
+a = 4
+b = 12
+
+if a > b: 
+    print('a is bigger than b')
+else: 
+    print('b is bigger than a')
+
+#There is also elif but in my opinion would be better to use a switch maybe
+
+#other way of writting the if are like this:
+c = 'Jhon'
+d = 'Chad'
+if c == 'Chad': print('hi, your name is' + c); print('how are you?')
+else: print("I don't know you ")
