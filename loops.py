d0c9508b9b5581300c65910c1c9ad21e4cf7910f
Hb_W <hugobarillasvi@hotmail.com>
2025-03-11 08:46:32 -0600

    loop while

---
+++ loops.py
@@ -0,0 +1,15 @@
+#Now learning about loops
+#For counting through items
+#while based on conditions
+
+#while 
+""" while <expresion>
+        statement """
+
+a = 0
+
+while (a < 5):
+    a = a + 1
+    print('How are you?')
+else:
+    print('Fine and you?')
