d2e5ca3a5766c1f5673b513c6ab6dcbca2f97883
Hb_W <hugobarillasvi@hotmail.com>
2025-03-11 20:36:35 -0600

    type conversion

---
+++ typeConversion.py
@@ -0,0 +1,19 @@
+#Just type of a value 
+
+a = '50'
+b = 3
+
+print('a is type: ', type(a) )
+print('b is type: ', type(b))
+
+#As I understand you can change the type really easy
+
+a = int(a)
+
+print(b + a)
+print('a now is type: ', type(a))
+
+#So it would be just like that
+
+b = str(b)
+print('now b is type: ', type(b))
